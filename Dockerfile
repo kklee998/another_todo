232520a2b4c1ba5deab5f275b8f8835a63c1bc14
kklee998 <kahkinlee998@gmail.com>
2019-03-17 14:19:32 +0800

    update run.py

--- Dockerfile
+++ Dockerfile
@@ -7,17 +7,11 @@ WORKDIR /app
 # Copy the current directory contents into the container at /app
 COPY . /app
 
-# Install any needed packages specified in requirements.txt
-RUN pip install --trusted-host pypi.python.org -r requirements.txt
-RUN ["python", "manage.py", "db", "init"]
-RUN ["python", "manage.py", "db", "migrate"]
-RUN ["python", "manage.py", "db", "upgrade"]
-
 # Make port 80 available to the world outside this container
 EXPOSE 80
 
-# Define environment variable
-ENV NAME World
+# Install any needed packages specified in requirements.txt
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
 
 # Run app.py when the container launches
 CMD ["gunicorn", "run:create_app('config')"]
--- run.py
+++ run.py
@@ -1,25 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
+from app import api_bp
+from Model import db
 
 
-def create_app(config_filename):
-    app = Flask(__name__)
-    app.config.from_object(config_filename)
+app = Flask(__name__)
+CORS(app)
+app.config.from_object("config")
+app.register_blueprint(api_bp, url_prefix='/api')
+db.init_app(app)
 
-    from app import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
 
-    from Model import db
-    db.init_app(app)
-
-    @app.route('/')
-    def its_alive():
-        return "Well, it's working..."
-
-    CORS(app)
-    return app
+@app.route('/')
+def its_alive():
+    return "Well, it's working..."
 
 
 if __name__ == "__main__":
-    app = create_app("config")
     app.run(debug=True)
